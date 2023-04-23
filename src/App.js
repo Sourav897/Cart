cdce3f5c23ce3a661e06d742294b85e053aa6f37
Sourav <Sauravpandeyaz897@gmail.com>
2023-04-23 20:07:54 +0530

    using state in class

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import CartItem from "./CartItem.js";
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
       <CartItem />
     </div>
   );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,46 @@
 import React from "react";
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Mobile Phone",
+      price: 999,
+      Qty: 1,
+      img: "",
+    };
+  }
+
   render() {
+    const { title, price, Qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img
-            src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F674010%2Fpexels-photo-674010.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26dpr%3D1%26w%3D500&tbnid=fY9reP86COngJM&vet=12ahUKEwjIvtKEicD-AhWs9nMBHWtHCKgQMygFegUIARDpAQ..i&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeautiful%2F&docid=B51x0PBR9KNzvM&w=500&h=667&itg=1&q=images&ved=2ahUKEwjIvtKEicD-AhWs9nMBHWtHCKgQMygFegUIARDpAQ"
-            style={styles.image}
-            alt=""
-          />
+          <img style={styles.image} alt="" />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>Phone</div>
-          <div style={{ color: "#777" }}>Rs 999</div>
-          <div style={{ color: "#777" }}>Qty: 1</div>
-          <div className="cart-item-actions">{/* Buttons */}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price} </div>
+          <div style={{ color: "#777" }}>Qty: {Qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              src="https://img.icons8.com/ios/1x/plus.png"
+              alt="increase"
+              className="action-icons"
+            />
+            <img
+              src="https://img.icons8.com/ios/1x/minus.png"
+              alt="decrease"
+              className="action-icons"
+            />
+
+            <img
+              src="https://img.icons8.com/carbon-copy/1x/filled-trash.png"
+              alt="delete"
+              className="action-icons"
+            />
+          </div>
         </div>
       </div>
     );
